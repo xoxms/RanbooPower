2258808605ca19c64b2fb201059f01f672a47740
Papajittan <79068718+ZzzMa111lma0@users.noreply.github.com>
2022-01-30 22:13:11 +0700

    Update Event.java

--- src/com/papajittan/ranboopower/Event.java
+++ src/com/papajittan/ranboopower/Event.java
@@ -5,11 +5,20 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.Objects;
 
 public class Event implements Listener {
+    @EventHandler
+    public void  onPlayerJoin(PlayerJoinEvent Event){
+        Player player = Event.getPlayer();
+        String pname = player.getName();
+        if(PluginSupporter.GetRanboo(pname) == null){
+            PluginSupporter.SetRanboo(pname, false);
+        }
+    }
     @EventHandler
     public void onBlockBreak(BlockBreakEvent Event){
         Player player = Event.getPlayer();
@@ -17,9 +26,9 @@ public class Event implements Listener {
         Material block = Event.getBlock().getType();
         PluginSupporter.RaNbOo data = PluginSupporter.GetRanboo(pname);
         if(data == null) return;
-        String datas = data.CheckRanboo;
-        if(Objects.equals(datas, "no")) return;
-        if(Objects.equals(datas, "yes")){
+        Boolean datas = data.CheckRanboo;
+        if(Objects.equals(datas, false)) return;
+        if(Objects.equals(datas, true)){
             if(player.getInventory().getItemInMainHand().getType() == Material.AIR){
                 Event.getBlock().getWorld().dropItemNaturally(Event.getBlock().getLocation(), new ItemStack(block));
                 Event.setCancelled(true);
